6b6eadedeb6bd91c23d76a400433782b66990064
Mayur <mayurzope555@gmail.com>
2020-10-10 15:01:49 +0530

    [Mayur]Added: Welcome to the Data Structure Problem

---
+++ src/main/java/com/bridgelabz/datastructures/MyNode.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.datastructures;
+
+public class MyNode {
+	public void printWelcomeMessage() {
+		System.out.println("Welcome to the Data Structure Problem");
+	}
+}
---
+++ src/test/java/com/bridgelabz/datastructures/MyNodeTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.datastructures;
+
+import org.junit.Test;
+
+public class MyNodeTest {
+	@Test
+	public void givenCallShouldPrintWelcomeMessage() {
+		MyNode myFirstNode = new MyNode();
+		myFirstNode.printWelcomeMessage();
+	}
+}
