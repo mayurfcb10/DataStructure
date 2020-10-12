f7d7933929c8bcb11e918d75474cf4fe4a9075c9
Mayur <mayurzope555@gmail.com>
2020-10-12 23:11:20 +0530

    [Mayur]Added: Search Key in the given Binary Search Tree

--- src/test/java/com/bridgelabz/datastructures/MyBinaryTree.java
+++ src/test/java/com/bridgelabz/datastructures/MyBinaryTree.java
@@ -4,10 +4,12 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
 	private MyBinaryNode<K> root;
 
+	/* add node */
 	public void add(K key) {
 		this.root = this.addRecursively(root, key);
 	}
 
+	/* add recursive */
 	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
 		if (current == null)
 			return new MyBinaryNode<>(key);
@@ -22,12 +24,31 @@ public class MyBinaryTree<K extends Comparable<K>> {
 		return current;
 	}
 
+	/* Get size */
 	public int getSize() {
 		return this.getSizeRecursive(root);
 	}
 
+	/* get recursive size */
 	private int getSizeRecursive(MyBinaryNode<K> current) {
 		return (current) == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+	}
+
+	/* Search Node in BST */
+	public boolean search(K key) {
+		return (searchRecursive(root, key) != null);
+	}
+
+	private MyBinaryNode<K> searchRecursive(MyBinaryNode<K> current, K key) {
+		if (current == null)
+			return null;
+		else if (current.key.compareTo(key) < 0)
+			return searchRecursive(current.right, key);
+		else if (current.key.compareTo(key) < 0)
+			return searchRecursive(current.left, key);
+		else
+			return current;
+
 	}
 
 }
